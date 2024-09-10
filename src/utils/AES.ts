b1de3b89093292d2fa8bb8560364ffa9ce0a470d
Ian <istreator@yahoo.com>
2024-09-09 21:56:39 -0500

    WIP: decryption and naming

--- src/utils/AES.ts
+++ src/utils/AES.ts
@@ -1,7 +1,7 @@
 const CRYPTO = window.crypto.subtle;
 
-const generateAESKey = async () => {
-  const key = await CRYPTO.generateKey(
+const generateCryptoKey = async () => {
+  const cryptoKey = await CRYPTO.generateKey(
     {
       name: "AES-GCM",
       length: 256,
@@ -10,60 +10,74 @@ const generateAESKey = async () => {
     ["encrypt", "decrypt"]
   );
 
-    const rawKey = await CRYPTO.exportKey("raw", key);
-    const base64Key = btoa(String.fromCharCode(...new Uint8Array(rawKey)));
-
-    console.log(rawKey);
-    console.log(String.fromCharCode(...new Uint8Array(rawKey)))
-
-    console.log(base64Key);
-    console.log(atob(base64Key));
-    // console.log(
-    //   atob(base64Key)
-    //     .split("")
-    //     .map((char) => char.charCodeAt(0))
-    // );
-  return key;
+  return { cryptoKey };
 };
 
-// Convert the secret key to a shareable/clipboard format.
-const AESKeyToBase64 = async (key: CryptoKey) => {
-  const rawKey = await CRYPTO.exportKey("raw", key);
+const cryptoKeyToBase64 = async (cryptoKey: CryptoKey) => {
+  const rawKey = await CRYPTO.exportKey("raw", cryptoKey);
+
   const base64Key = btoa(String.fromCharCode(...new Uint8Array(rawKey)));
-  console.log(base64Key);
-  return base64Key;
+  return { base64Key };
+};
+const base64ToCryptoKey = async (base64Key: string) => {
+  const decodedKey = Uint8Array.from(atob(base64Key), (c) => c.charCodeAt(0));
+
+  const symmetricKey = await CRYPTO.importKey(
+    "raw",
+    decodedKey,
+    { name: "AES-GCM", length: 256 },
+    true,
+    ["encrypt", "decrypt"]
+  );
+  console.log(symmetricKey);
 };
 
-const encryptData = async (key: CryptoKey, data: string) => {
+const encryptData = async (cryptoKey: CryptoKey, data: string) => {
   const iv = crypto.getRandomValues(new Uint8Array(12));
-  const encoder = new TextEncoder();
-  const encodedData = encoder.encode(data);
+  const encodedData = new TextEncoder().encode(data);
+  // const encodedData = encoder.encode(data);
   const encryptedData = await CRYPTO.encrypt(
     {
       name: "AES-GCM",
       iv,
     },
-    key,
+    cryptoKey,
     encodedData
   );
-  return { iv, encryptedData };
+  console.log(encryptedData);
+  console.log(new Uint8Array(encryptedData));
+  const combinedBuffer = new Uint8Array(iv.length + encryptedData.byteLength);
+  combinedBuffer.set(iv, 0);
+  combinedBuffer.set(new Uint8Array(encryptedData), iv.length);
+  // console.log(combinedBuffer);
+
+  const base64Cipher = btoa(String.fromCharCode(...combinedBuffer));
+  // return { iv, encryptedData };
+  return { base64Cipher };
 };
 
 const decryptData = async (
-  key: CryptoKey,
+  cryptoKey: CryptoKey,
   iv: Uint8Array,
   encryptedData: ArrayBuffer
 ) => {
-  const decoder = new TextDecoder();
   const decryptedData = await CRYPTO.decrypt(
     {
       name: "AES-GCM",
       iv,
     },
-    key,
+    cryptoKey,
     encryptedData
   );
-  return decoder.decode(decryptedData);
+  const decoder = new TextDecoder();
+  const originalData = decoder.decode(decryptedData);
+  return { originalData };
 };
 
-export { generateAESKey, encryptData, decryptData };
+export {
+  generateCryptoKey,
+  cryptoKeyToBase64,
+  base64ToCryptoKey,
+  encryptData,
+  decryptData,
+};
