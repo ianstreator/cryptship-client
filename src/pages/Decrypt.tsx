1c079c6339289cc0df5ceeb967d51869fb926708
Ian <istreator@yahoo.com>
2024-09-09 21:22:41 -0500

    WIP: setup useParams and fetching. simple input for key.

--- src/pages/Decrypt.tsx
+++ src/pages/Decrypt.tsx
@@ -1,12 +1,28 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { decryptData } from "../utils/AES";
 
 function Decrypt() {
-  const { id: redisCipherKey } = useParams();
-  console.log(redisCipherKey);
+  const { id: cipherKey } = useParams();
+  const BASE_URL = "http://localhost:3000/decrypter/"
+
+  const [cipher, setCipher] = useState("")
+  const [message, setMessage] = useState("")
+  const [decryptKey, setDecryptKey] = useState("")
+
+
+  const getChiper = async () => {
+    const res = await fetch(BASE_URL + cipherKey)
+    const cipher = await res.json()
+    setCipher(cipher)
+  }
+
+  useEffect(() => {
+    getChiper()
+  }, [])
   return (
     <>
-      <div>Decrypt</div>
+      <input type="text" name="decrypt-key" placeholder="Paste decryption key here." />
     </>
   );
 }
