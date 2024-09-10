54265e624978d8eaf8019a174e29e3b9fb42db49
Ian <istreator@yahoo.com>
2024-09-09 21:24:53 -0500

    WIP: setup form handler, post request, and response UI.

--- src/pages/Encrypt.tsx
+++ src/pages/Encrypt.tsx
@@ -1,57 +1,109 @@
-import { FormEvent } from "react";
-import { encryptData, generateAESKey } from "../utils/AES";
+import { FormEvent, useEffect, useState } from "react";
+import { encryptData, generateSymmetricKey, symmetricKeyToShareableKey, shareableKeyToSymmetricKey } from "../utils/AES";
 
 function Encrypt() {
+  const BASE_URL = "http://localhost:5173/decrypt/"
+
+  const [decryptUrl, setDecryptUrl] = useState("")
+  const [decryptKey, setDecryptKey] = useState("")
+
+
+  useEffect(() => {
+    console.log(decryptUrl)
+    console.log(decryptKey)
+  }, [decryptUrl, decryptKey, setDecryptUrl, setDecryptKey])
+
+
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const form = e.target as HTMLFormElement;
-    const inputElement = form.elements.namedItem("data") as HTMLInputElement;
-    const inputValue = inputElement.value;
+    const cipherElement = form.elements.namedItem("cipher") as HTMLInputElement;
+    const plainTextValue = cipherElement.value;
 
-    if (inputValue === "") return;
+    const ttlElement = form.elements.namedItem("ttl") as HTMLInputElement;
+    const ttlValue = ttlElement.value;
 
-    const key = await generateAESKey();
+    if (plainTextValue === "") return;
 
-    const { encryptedData, iv } = await encryptData(key, inputValue);
+    const { symmetricKey } = await generateSymmetricKey();
+    const { shareableKey } = await symmetricKeyToShareableKey(symmetricKey);
 
-    // const res = await fetch("https://localhost:4000",)
-    console.log(encryptedData, iv, key);
-    console.log(new Uint8Array(encryptedData));
+    console.log(symmetricKey, shareableKey)
+
+    const cipher = await encryptData(symmetricKey, plainTextValue);
+
+    const res = await fetch("http://localhost:3000/encrypter", {
+      method: "POST",
+      headers: {
+        "Content-Type":
+          "application/json"
+      },
+      body: JSON.stringify({ cipher, ttl: ttlValue })
+    });
+
+
+    const { cipherKey } = await res.json()
+
+    if (res.status !== 200) return
+
+    console.log(cipherKey)
+
+    setDecryptUrl(BASE_URL + cipherKey)
+    setDecryptKey(shareableKey)
   };
 
   return (
-    <>
-      <div>Encrypt</div>
-      <form action="submit" onSubmit={(e) => handleSubmit(e)}>
-        <p>Default expiration 10 minutes.</p>
-        <input
-          type="number"
-          min={10}
-          max={60}
-          defaultValue={10}
-          step={5}
-          placeholder="Enter minutes till expiration of data"
-        />
-        <p>
-          Once you click encrypt, your data will be encrypted in the browser.
-          Only the encryption will be stored on our server for the length of
-          time you chose above.
-        </p>
-        <br />
-        <p>
-          A link to our decrypt page will be generated as well as the key needed
-          for decryption. Send both of these to the recipient.
-        </p>
-        <input
-          style={{ width: 300 }}
-          type="text"
-          name="data"
-          placeholder="Paste data to be encrypted here"
-        />
-        <button type="submit">ENCRYPT</button>
+    <div className="p-10">
+      <form action="submit" onSubmit={(e) => handleSubmit(e)} className="h-60 flex flex-col justify-around items-center">
+        <div className="flex">
+          <label>Set how long the encryption will last in minutes.</label>
+          <input
+            name="ttl"
+            type="number"
+            min={10}
+            max={60}
+            defaultValue={10}
+            step={5}
+            className="text-center"
+          />
+        </div>
+
+        <div className="flex ">
+          <input
+            style={{ width: 300 }}
+            type="text"
+            name="cipher"
+            placeholder="Enter data to be encrypted here."
+          />
+          <button type="submit">ENCRYPT</button>
+
+        </div>
+
       </form>
-    </>
+
+      {(decryptUrl && decryptKey) &&
+        <div className="p-8 m-8 bg-black/50 flex flex-col items-center justify-center">
+          <h1 className=" text-xl">Share the following with the decrypter</h1>
+
+          <div className="flex flex-col items-start">
+            <div className="flex flex-row m-2">
+              <p className="p-2 m-1 select-none">URL: </p>
+              <p className="p-3 px-6 rounded-md max-w-fit justify-center bg-black/50">{decryptUrl}</p>
+            </div>
+            <div className="flex flex-row m-2">
+              <p className="p-2 m-1 select-none">KEY: </p>
+              <p className="p-3 px-6 rounded-md max-w-fit justify-center bg-black/50">
+                {decryptKey}
+              </p>
+
+            </div>
+
+          </div>
+
+        </div>
+      }
+    </div>
   );
 }
 
